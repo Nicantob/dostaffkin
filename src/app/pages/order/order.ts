7a48aee2103369b11cfb07b6ff6b6eea58fa34e9
Anton Boytsov <147425909+Nicantob@users.noreply.github.com>
2026-06-20 20:48:05 +0000

    Buid

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -3,6 +3,7 @@ import { Header } from '../../header/header';
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
+import { DeliveryApi } from '../../services/delivery-api';
 
 declare var ymaps: any;
 
@@ -25,8 +26,9 @@ export class Order {
 
   public orderId: any = signal(null);
   public calculationResult: any = signal(null);
+  public isCalculating: any = signal(false);
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private deliveryApi: DeliveryApi) {
     this.routeForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['', Validators.required],
@@ -64,8 +66,10 @@ export class Order {
 
   public calculate() {
     this.calculationResult.set(null);
+    this.isCalculating.set(true);
 
     if (!this.map || this.routeForm.invalid) {
+      this.isCalculating.set(false);
       return;
     }
 
@@ -115,6 +119,8 @@ export class Order {
         });
       } catch (err) {
         this.failedCalculation();
+      } finally {
+        this.isCalculating.set(false);
       }
     });
 
@@ -123,6 +129,7 @@ export class Order {
 
   private failedCalculation() {
     this.calculationResult.set(null);
+    this.isCalculating.set(false);
     alert('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
   }
 
@@ -149,7 +156,13 @@ export class Order {
       createdAt: new Date().toISOString()
     };
 
-    console.log(payload);
-    this.orderId.set(1);
+    this.deliveryApi.createDelivery(payload).subscribe((response) => {
+      if ('error' in response) {
+        alert(response.error);
+        return;
+      }
+
+      this.orderId.set(response.id);
+    });
   }
 }
